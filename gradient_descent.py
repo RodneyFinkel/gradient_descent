186b03ad92597139f4762b5ffe156a91a4f9fc90
Rodney Finkel <111357994+RodneyFinkel@users.noreply.github.com>
2023-09-26 17:23:48 +0300

    first commit

---
+++ gradient_descent.py
@@ -0,0 +1,26 @@
+import numpy as np 
+
+class LinearRegression:
+    def __init__(self, learning_rate = 1e-3, n_iters = 1000):
+        pass
+    
+    def _init_params(self):
+        pass
+    
+    def _update_params(self, dw, db):
+        pass
+    
+    def _get_prediction(self, X):
+        pass
+    
+    def _get_gradients(self, X, y, y_pred):
+        pass
+    
+    def fit(self, X, y):
+        pass
+    
+    def predict(self, X):
+        pass
+    
+    
+    
